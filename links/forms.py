1e13a897dce0d789432b4351cdae71298b194779
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-11-07 17:04:01 +0300

    done

--- links/forms.py
+++ links/forms.py
@@ -7,3 +7,9 @@ class LinkForm(forms.ModelForm):
     class Meta:
         model = Link
         fields = ['name', 'url', 'slug']
+
+
+# class LinkForm(forms.Form):
+#     name = forms.CharField(max_length=100)
+#     url = forms.URLField(max_length=200)
+#     slug = forms.SlugField(required=False)
